656b522344a7065ab284d69a48ce5a9041a99641
Patrick Pichler <git@patrickpichler.dev>
2024-06-27 14:42:02 +0200

    Rework sample to actually working code

--- examples/simple/hello.bpf.c
+++ examples/simple/hello.bpf.c
@@ -4,14 +4,15 @@
 
 char LICENSE[] SEC("license") = "GPL";
 
-SEC("xdp")
+SEC("tracepoint/sched_process_fork")
 int hello(void *ctx) {
+
   __u64 val = bpf_get_current_pid_tgid();
   __u32 pid = val >> 32;
 
-  if (pid == 32) {
-    return XDP_DROP;
+  if (pid == 13) {
+    bpf_printk("fork detected");
   }
 
-  return XDP_PASS;
+  return 0;
 }
